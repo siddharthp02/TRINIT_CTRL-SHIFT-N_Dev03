999b5151029ce35f33e70c05182d06563a765824
Siddharth P <siddharth.padmakumar2002@gmail.com>
2022-01-29 20:24:43 +0530

    no

--- Proj/client/src/pages/home/index.js
+++ Proj/client/src/pages/home/index.js
@@ -1,15 +1,103 @@
-import React from 'react'
+import React, {Component} from 'react'
 import "./home.css"
+import axios from 'axios';
 
+class Home extends Component {
 
-function Home() {
-    return (
-        <div className = "body">
-            <div id="home" className="main_box"  style = {{textAlign:"center"}}>
-                <h1>Welcome to OUR PROJECT</h1>
-            </div>
-        </div>
-    )
+	state = {
+
+	// Initially, no file is selected
+	selectedFile: null
+	};
+	
+	// On file select (from the pop up)
+	onFileChange = event => {
+	
+	// Update the state
+	this.setState({ selectedFile: event.target.files[0] });
+	var reader = new FileReader();
+    reader.onload = function(event) {
+    // The file's text will be printed here
+    console.log(event.target.result)
+    };
+
+    reader.readAsText(event.target.files[0]);
+	};
+	
+	// On file upload (click the upload button)
+	onFileUpload = () => {
+	
+	// Create an object of formData
+	const formData = new FormData();
+	
+	// Update the formData object
+	formData.append(
+		"myFile",
+		this.state.selectedFile,
+		this.state.selectedFile.name
+	);
+	
+	// Details of the uploaded file
+	console.log(this.state.selectedFile);
+	
+	// Request made to the backend api
+	// Send formData object
+	axios.post("api/uploadfile", formData);
+	};
+	
+	// File content to be displayed after
+	// file upload is complete
+	fileData = () => {
+	
+	if (this.state.selectedFile) {
+		
+		return (
+		<div>
+			<h2>File Details:</h2>
+			
+<p>File Name: {this.state.selectedFile.name}</p>
+
+			
+<p>File Type: {this.state.selectedFile.type}</p>
+
+			
+<p>
+			Last Modified:{" "}
+			{this.state.selectedFile.lastModifiedDate.toDateString()}
+			</p>
+
+		</div>
+		);
+	} else {
+		return (
+		<div>
+			<br />
+			<h4>Choose before Pressing the Upload button</h4>
+		</div>
+		);
+	}
+	};
+	
+	render() {
+	
+	return (
+		<div>
+			<h1>
+			GeeksforGeeks
+			</h1>
+			<h3>
+			File Upload using React!
+			</h3>
+			<div>
+				<input type="file" onChange={this.onFileChange} />
+				<button onClick={this.onFileUpload}>
+				Upload!
+				</button>
+			</div>
+		{this.fileData()}
+		</div>
+	);
+	}
 }
 
-export default Home
+export default Home;
